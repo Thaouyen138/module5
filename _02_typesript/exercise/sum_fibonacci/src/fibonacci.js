703a2331a2800645d66329a01f5e251fe0735e06
Thaouyen138 <123uyen234@gmail.com>
2022-09-21 14:29:43 +0700

    exercise-02-typescript

---
+++ _02_typesript/exercise/sum_fibonacci/src/fibonacci.js
@@ -0,0 +1,19 @@
+var fibonacciArray = [0, 1];
+var total = 1;
+var a = 0;
+var b = 1;
+var c = 0;
+while (true) {
+    if (a + b == c) {
+        fibonacciArray.push(c);
+        a = b;
+        b = c;
+        total += c;
+    }
+    c++;
+    if (fibonacciArray.length == 20) {
+        break;
+    }
+}
+console.log(fibonacciArray);
+console.log(total);
