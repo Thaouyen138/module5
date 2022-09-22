2df1496913820660bfad4dd5d83c3b423a60ed67
Thaouyen138 <123uyen234@gmail.com>
2022-09-22 16:47:00 +0700

    exercise

--- _03_angular_overview/exercise/calculator-simple/calculator-app/src/app/calculator/calculator.component.ts
+++ _03_angular_overview/exercise/calculator-simple/calculator-app/src/app/calculator/calculator.component.ts
@@ -6,10 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
+  result= '';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  add(number: number) {
+    this.result+= ''+ number;
+  }
+
+
+  math(s: string) {
+    switch (s){
+      case '+':
+        this.result += '+';
+        break;
+      case '-':
+        this.result += '-';
+        break;
+      case '*':
+        this.result += '*';
+        break;
+      case '/':
+        this.result += '/';
+        break;
+    }
+
+  }
+
+  getResult() {
+    this.result = eval(this.result)
+  }
+
+  delete() {
+    this.result = '';
+  }
 }
